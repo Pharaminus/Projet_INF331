4b608997460fbe3fd300581dbfa388ec0ffbc519
Pharaminus <pharaminusbaba@gmail.com>
2024-01-02 23:48:57 +0100

    version 1.1.0 model de prediction terminer avec premier test reussi

--- src/main/java/com/studor/orientation_student/manager/controller/suggestionCourRestController/ObjectifRestController.java
+++ src/main/java/com/studor/orientation_student/manager/controller/suggestionCourRestController/ObjectifRestController.java
@@ -1,139 +1,70 @@
 package com.studor.orientation_student.manager.controller.suggestionCourRestController;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.studor.orientation_student.entities.suggestionCourEntities.EmploiTemps;
-import com.studor.orientation_student.entities.suggestionCourEntities.ModelSuggestionCour;
+import com.studor.orientation_student.entities.suggestionCourEntities.DataModel;
 import com.studor.orientation_student.manager.service.suggestionCour.ClassificationData;
-import com.studor.orientation_student.manager.service.suggestionCour.SuggestionCourClassificationService;
+import com.studor.orientation_student.manager.service.suggestionCour.DataGeneration;
+import com.studor.orientation_student.manager.service.suggestionCour.GetAbsoluePath;
 
+import weka.classifiers.Classifier;
 import weka.core.Instances;
+import weka.core.converters.ConverterUtils.DataSource;
 
 @RestController
-@RequestMapping("/objectifRest")
 public class ObjectifRestController {
-    private SuggestionCourClassificationService classificationService;
 
-    public ObjectifRestController(SuggestionCourClassificationService classificationService) {
-        this.classificationService = classificationService;
+    // @Autowired
+    // private ClassificationData classificationData;
+
+    // @Autowired
+    // private Classifier modele; // Votre modèle préalablement entraîné
+
+    @PostMapping("/classification")
+    public String effectuerClassification(@ModelAttribute DataModel dataModel) throws IOException {
+
+        // generation du fichier pour la recherche de la class
+        dataModel.setNomEmpoitemps("?");
+        dataModel.setMoyenneScolaire(dataModel.getMoyenneScolaire().replace(" ", "_"));
+        dataModel.setStatutProfessionnel(dataModel.getStatutProfessionnel().replace(" ", "_"));
+        List<DataModel> dataModels = new ArrayList<>();
+        dataModels.add(dataModel);
+        DataGeneration dataGeneration = new DataGeneration();
+        dataGeneration.writeARFFFile(dataModels, 1);
+
+        Classifier modele = ClassificationData.classifyData();
+        String predictedClassName = " aucune class trouver";
+        Instances testData;
+        try {
+
+            GetAbsoluePath getAbsoluePath = new GetAbsoluePath();
+            String path = getAbsoluePath.getAbsolutePathOfFile();
+            path = path + "/data/fichier_test.arff";
+
+            // Charger les données de test à partir d'un fichier ARFF
+            DataSource testSource = new DataSource(path);
+            testData = testSource.getDataSet();
+
+            // Définir l'attribut de classe pour les données de test
+            testData.setClassIndex(testData.numAttributes() - 1);
+
+            // Effectuer la classification sur les données de test
+            for (int i = 0; i < testData.numInstances(); i++) {
+                double predictedClass = modele.classifyInstance(testData.instance(i));
+                predictedClassName = testData.classAttribute().value((int) predictedClass);
+                System.out.println("Instance " + (i + 1) + ": Classe prédite = " + predictedClassName);
+            }
+            return "<h1> L'emploi de temps predit est > : " + predictedClassName +" </h1>";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erreur lors de la classification.\n" + e.getMessage() + " \n";
+        }
     }
 
-    // @PostMapping("/classer")
-    @GetMapping("/classer")
-
-    public String classerParAbreDecisionnel() {
-        // public String classerParAbreDecisionnel(@RequestBody ModelSuggestionCour
-        // model){
-        // Création des exemples d'entraînement et de test
-        // List<ModelSuggestionCour> trainingExamples = new ArrayList<>();
-        // trainingExamples.add(new ModelSuggestionCour("cenior", "passable",new EmploiTemps((long) 12,"facile" , "defaut")));
-        // trainingExamples.add(new ModelSuggestionCour("cadet", "bien",new EmploiTemps((long) 20,"difficile" , "defaut")));
-
-        // List<ModelSuggestionCour> testExamples = new ArrayList<>();
-        // testExamples.add(new ModelSuggestionCour("cenior", "tres bien",new EmploiTemps((long) 34,"facile" , "personalise")));
-        // testExamples.add(new ModelSuggestionCour("professionnel", "passable",new EmploiTemps((long) 12,"facile" , "defaut")));
-
-        // // Création de l'objet ClassificationData
-        // ClassificationData data = new ClassificationData(trainingExamples, testExamples);
-
-        // // Utilisation des données d'entraînement
-        // Instances trainingData = data.getTrainingData();
-        // System.out.println("Nombre d'instances dans les données d'entraînement : " + trainingData.numInstances());
-
-        // // Utilisation des données de test
-        // Instances testData = data.getTestData();
-        // System.out.println("Nombre d'instances dans les données de test : " + testData.numInstances());
-
-
-        // Création des exemples d'entraînement et de test
-        // List<TrainingExample> trainingExamples = new ArrayList<>();
-        // trainingExamples.add(new TrainingExample(1.0, 2.0, "class1"));
-        // trainingExamples.add(new TrainingExample(3.0, 4.0, "class2"));
-
-        // List<TestExample> testExamples = new ArrayList<>();
-        // testExamples.add(new TestExample(2.0, 3.0));
-        // testExamples.add(new TestExampel(4.0, 5.0));
-
-        // // Création de l'objet ClassificationData
-        // ClassificationData data = new ClassificationData(trainingExamples, testExamples);
-
-        // // Utilisation des données d'entraînement
-        // Instances trainingData = data.getTrainingData();
-        // System.out.println("Nombre d'instances dans les données d'entraînement : " + trainingData.numInstances()+ " ");
-
-        // Utilisation des données de test
-        // Instances testData = data.getTrainingData()     ;
-        
-        
-        
-        return "ok";
-    }
-
-    public class TestExample{
-        private Double taille;
-        private Double poids;
-        // private String teint;
-        public TestExample() {
-        }
-        public TestExample(Double taille, Double poids) {
-            this.taille = taille;
-            this.poids = poids;
-            // this.teint = teint;
-        }
-        public Double getTaille() {
-            return taille;
-        }
-        public void setTaille(Double taille) {
-            this.taille = taille;
-        }
-        public Double getPoids() {
-            return poids;
-        }
-        public void setPoids(Double poids) {
-            this.poids = poids;
-        }
-        
-
-        
-    }
-
-    public class TrainingExample{
-        private Double taille;
-        private Double poids;
-        private String teint;
-        public TrainingExample() {
-        }
-        public TrainingExample(Double taille, Double poids, String teint) {
-            this.taille = taille;
-            this.poids = poids;
-            this.teint = teint;
-        }
-        public Double getTaille() {
-            return taille;
-        }
-        public void setTaille(Double taille) {
-            this.taille = taille;
-        }
-        public Double getPoids() {
-            return poids;
-        }
-        public void setPoids(Double poids) {
-            this.poids = poids;
-        }
-            public String getTeint() {
-                return teint;
-        }
-        public void setTeint(String teint) {
-            this.teint = teint;
-        }
-
-        
-    }
 }
